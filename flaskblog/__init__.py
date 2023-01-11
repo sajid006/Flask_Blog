5422784f4b87d03f7eaf1577c5cbf715b92d5cb8
sajidhasan006 <sajid.hasan@cefalo.com>
2023-01-11 15:32:20 +0600

    Added forgot password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '38956b4d19d5dd97ccf7eab26effcddb'
@@ -10,5 +12,13 @@ db = SQLAlchemy(app)
 app.app_context().push()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'sh3176183@gmail.com'
+
+mail = Mail(app)
 
 from flaskblog import routes
