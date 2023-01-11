031098f29b51badf40e8d919c0e7dbeeb411e122
sajidhasan006 <sajid.hasan@cefalo.com>
2023-01-11 15:35:00 +0600

    Check

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -18,7 +18,7 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'sh3176183@gmail.com'
-
+app.config['MAIL_PASSWORD'] = 
 mail = Mail(app)
 
 from flaskblog import routes
